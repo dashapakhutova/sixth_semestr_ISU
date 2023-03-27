9f78952c6300abb17b2b85fcd5d4ac86f216743a
dashapakhutova <114560525+dashapakhutova@users.noreply.github.com>
2023-03-27 23:36:32 +0800

    Update carwifi.py

--- microcontrollers/CarControlWifi/carwifi.py
+++ microcontrollers/CarControlWifi/carwifi.py
@@ -1,1 +1,38 @@
-^(
+import machine
+import time
+
+class CarController:
+    def __init__(self, serial_port='/dev/ttyUSB0', baud_rate=115200):
+        self.ser = machine.UART(0, baud_rate)
+        self.ser.init(baudrate=baud_rate, bits=8, parity=None, stop=1, tx=machine.Pin(1), rx=machine.Pin(3))
+
+    def forward(self):
+        command = "f"
+        self.ser.write(command + '\n')
+        time.sleep(0.1)
+
+    def backward(self, speed):
+        command = "b"
+        self.ser.write(command + '\n')
+        time.sleep(0.1)
+        
+    def turn_left(self, speed):
+        command = "l"
+        self.ser.write(command + '\n')
+        time.sleep(0.1)
+        
+    def turn_right(self, speed):
+        command = "r"
+        self.ser.write(command + '\n')
+        time.sleep(0.1)
+        
+    def stop(self):
+        command = "s"
+        self.ser.write(command + '\n')
+        time.sleep(0.1)
+
+car = CarController()
+
+# car.forward()
+# car.stop()
+# car.backward()
